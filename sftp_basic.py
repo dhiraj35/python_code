dfd8dfdbbc9a09372b6de8c35148a65e575df58c
dhiraj35 <75352708+dhiraj35@users.noreply.github.com>
2022-07-28 11:34:14 +0530

    Update sftp_basic.py

--- sftp_basic.py
+++ sftp_basic.py
@@ -2,4 +2,6 @@ l = sftp.listdir() // get dir list
 sftp.put(tempFile, uploadFileUrl) // upload file 
 size = sftp.stat(filename ).st_size // get file size 
  sftp.remove(uploadFileUrl) // remove file 
+         sftp.makedirs("ADMandUpload/", mode=777)  # make dir 
+
  
